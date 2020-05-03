789aeddf8394bdb38cc231db515cb273be8aa136
Braedon Gough <braedon@pleo.io>
2020-05-03 16:12:01 +0200

    first commit

--- src/client/App.js
+++ src/client/App.js
@@ -1,23 +1,67 @@
-import React, { Component } from 'react';
-import './app.css';
-import ReactImage from './react.png';
+import React, { Component } from "react";
+import {
+  BrowserRouter,
+  Route,
+  Switch,
+  Link,
+  useParams,
+  Redirect,
+} from "react-router-dom";
+import "./app.css";
 
+const Profile = ({ users }) => {
+  const { userId } = useParams();
+
+  const user = users.find((user) => user.id === Number(userId));
+
+  return (
+    <div>
+      <h1>{user.name}</h1>
+      <h3>{user.favouriteColor}</h3>
+      <Link to="/">back home</Link>
+    </div>
+  );
+};
+
+const Home = (props) => (
+  <div>
+    <h1>List of users</h1>
+    <div>
+      {props.users.map((user) => (
+        <Link key={user.id} to={`/${user.id}`}>
+          <div>{user.name}</div>
+        </Link>
+      ))}
+    </div>
+  </div>
+);
 export default class App extends Component {
-  state = { username: null };
+  state = { users: [] };
 
   componentDidMount() {
-    fetch('/api/getUsername')
-      .then(res => res.json())
-      .then(user => this.setState({ username: user.username }));
+    fetch("/api/users")
+      .then((res) => res.json())
+      .then(({ users }) => this.setState({ users }));
   }
 
   render() {
-    const { username } = this.state;
+    const { users } = this.state;
+
+    if (!this.state.users.length) {
+      return <div>Loading...</div>;
+    }
+
     return (
-      <div>
-        {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
-        <img src={ReactImage} alt="react" />
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/:userId">
+            <Profile users={users} />
+          </Route>
+          <Route path="/">
+            <Home users={users} exact />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,30 @@
-const express = require('express');
-const os = require('os');
+const express = require("express");
 
 const app = express();
 
-app.use(express.static('dist'));
-app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+app.use(express.static("dist"));
+app.get("/api/users", (req, res) =>
+  res.send({
+    users: [
+      {
+        id: 1,
+        name: "Braedon",
+        favouriteColor: "green",
+      },
+      {
+        id: 2,
+        name: "Younes",
+        favouriteColor: "orange",
+      },
+      {
+        id: 3,
+        name: "Fernando",
+        favouriteColor: "purple",
+      },
+    ],
+  })
+);
 
-app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
+app.listen(process.env.PORT || 8080, () =>
+  console.log(`Listening on port ${process.env.PORT || 8080}!`)
+);
